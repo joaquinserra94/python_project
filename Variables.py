8ba285935f3edd15154cc1785e16da3bc8461a2c
Joaquin <joaquinserra94@gmail.com>
2024-03-13 05:35:03 +0100

    Variables

---
+++ Variables.py
@@ -0,0 +1,17 @@
+nombre1 = "joaquin"
+print(nombre1)
+
+edad = 28
+edad2 = 15
+
+print(edad + edad2)
+
+nombre = input("Dime tu nombre")
+print("tu nombre es: " + nombre)
+
+#-------------------------------------
+
+nombre = "Julia"
+apellido = "Roberts"
+
+nombrecompleto = (nombre + apellido)
